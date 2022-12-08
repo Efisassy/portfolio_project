611e841a3bfc8ce1738e332e3baa37534f03e739
efisassy <eefyaasare@gmail.com:wq>
2022-12-08 19:48:42 +0000

    first changes

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>start</title>
+    <link rel="stylesheet" href="style.css">
+   
+</head>
+<body>
+<div class="forrm">
+    <form>
+        <label for="fname">First name:</label><br>
+        <input type="text" id="fname" name="fname"><br>
+        <label for="lname">Last name:</label><br>
+        <input type="text" id="lname" name="lname">
+    </form>
+</div>
+</body>
+
+    <?php
+    echo "My first PHP script!";
+    ?> 
+</html>
