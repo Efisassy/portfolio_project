1b98ec4402bbc1d38e3a359ec46f35da72d50feb
efisassy <eefyaasare@gmail.com:wq>
2022-12-11 14:36:19 +0000

    added (API, JQUERY)

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-document.getElementById("demo").innerHTML = ("anything");
+$(document).ready(function () {
+    // Getting data from the form
+    let email = "";
+    let pwd = "";
+    const url = 'http://localhost:3000/api';
+
+    // Submitting the  form on click
+    $("#loginForm").submit(function () {
+        event.preventDefault();
+        email = $("#email").val();
+        pwd =  $("#pwd").val();
+        let data = {
+            password: pwd,
+            email: email
+        }
+        console.log(data);
+        $.ajax(url, {
+            data: JSON.stringify(data),
+            contentType: 'application/json',
+            type:'POST',
+            success: function(){
+                console.log("success");
+            }
+        })
+    });
+})
